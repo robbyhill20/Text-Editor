cf186070e4276c2b37e2bfb2c6a87afb26a4b822
Robby Hill <robbyhill20@yahoo.com>
2022-03-07 12:11:29 -0800

    heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -10,26 +11,31 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-export const putDb = async (id, content) => {
-  console.log('Database is updated!');
+
+// Method that takes some content and adds it to the IndexedDB database using the idb module
+export const putDb = async (content) => {
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id, content });
-
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data successfully saved to the database', result);
-  
+  console.log('🚀 - data saved to the database', result.value);
 };
-//TO DO: 
+
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  console.log('getting all data');
+  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
-
+  const request = store.get(1);
   const result = await request;
-  console.log('Data:', result);
-}
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
+};
+
 initdb();
