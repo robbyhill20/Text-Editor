38cb66f077bf590364d43aaa141d3a50bf95cc50
Robby Hill <robbyhill20@yahoo.com>
2022-03-07 11:50:28 -0800

    fixing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,41 +1,35 @@
 import { openDB } from 'idb';
-
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectstoredNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectstored('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
+export const putDb = async (id, content) => {
+  console.log('Database is updated!');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id, content });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>{
-console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const txt = jateDb.transaction('jate', 'readwrite');
-  const stored = txt.objectstored('jate');
-  const request = stored.put({ id: 1, value: content });
   const result = await request;
-  console.log(' to the database', result.value)
-}
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const txt = jateDb.transaction('jate', 'readonly');
-  const stored = txt.objectstored('jate');
-  const request = stored.get(1);
-  const jateDb = await openDB('jate', 1);
+  console.log('Data successfully saved to the database', result);
   
+};
+//TO DO: 
+export const getDb = async () => {
+  console.log('getting all data');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
   const result = await request;
-  result
-   ? console.log('got data', result.value)
-  : console.log('nothing found');
- 
-  return result?.value;
+  console.log('Data:', result);
 }
 initdb();
